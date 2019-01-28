1e31af388e34738ecd98324905655c185a2f300b
tonihappe <46331585+tonihappe@users.noreply.github.com>
2019-01-28 12:59:34 +0100

    Create sketch_jan28a.ino

---
+++ Projektordner/19_01_28/sketch_jan28a/sketch_jan28a.ino
@@ -0,0 +1,29 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+#define ANZAHL 59
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIN,ANZAHL,NEO_GRBW + NEO_KHZ800);
+
+void setup() {
+  // put your setup code here, to run once:
+  strip.begin();
+  strip.show();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ for(int n=0;n<PIN;n++)
+ {
+  strip.setPixelColor(n,255,255,255);
+ }
+  strip.show();
+delay(1000);
+ for(int n=0;n<PIN;n++)
+ {
+  strip.setPixelColor(n,0,0,0);
+ }
+  strip.show();
+
+}
